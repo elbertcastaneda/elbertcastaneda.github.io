57176f3d18d37a664b0a0b8531edc0b54499d7fa
Elbert Castañeda <elbert@sbxretail.com>
2019-11-08 11:46:12 -0600

    Movies structure layout => ocr.filmFestival

--- watch-it-outside-boston-edition/main.js
+++ watch-it-outside-boston-edition/main.js
@@ -1,10 +1,49 @@
 (function() {
   'strict mode';
-  $('.nav-link[href^="#"],a.dropdown-item[href^="#"]', 'header').click('click', function(event) {
+  $('a.nav-link[href^="#"]:not([href="#"]),a.dropdown-item[href^="#"]', 'header').click('click', function(event) {
     var href = $(this).attr('href');
-    $('main').animate({
-      scrollTop: $(href).offset().top - 180
-    }, 1500);
-    event.preventDefault();
+    var $section2Scroll = $(href);
+    var $main = $('main');
+    var sectionTop = $section2Scroll.offset().top - 180;
+    var currentScroll = $main.scrollTop();
+    var moveScrollStopTo = sectionTop + currentScroll;
+
+    $('.navbar-collapse').collapse('hide');
+
+    $main.animate({
+      scrollTop: moveScrollStopTo
+    }, Math.abs(currentScroll -  moveScrollStopTo), () => {
+    });
+    // event.preventDefault();
   });
+
+  function leftSlide(tab){
+    var $tab = $(tab);
+    $tab.removeClass('animated slideInRight');
+    $tab.addClass('animated slideInLeft');
+  }
+
+  function rightSlide(tab){
+    var $tab = $(tab);
+    $tab.removeClass('animated slideInLeft');
+    $tab.addClass('animated slideInRight');
+  }
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      var url = String(e.target);
+      var $fromTarget = $(e.relatedTarget);
+      var $toTarget = $(e.target);
+      var fromDayEvent = Number($fromTarget.attr('id').replace(/(movies-)|(th-tab)/g, ''));
+      var toDayEvent = Number($toTarget.attr('id').replace(/(movies-)|(th-tab)/g, ''));
+      console.log(fromDayEvent, toDayEvent);
+      var tab = $(this).attr('href');
+
+      if (fromDayEvent < toDayEvent){
+        rightSlide(tab);
+        console.log('left => right');
+      } else {
+        console.log('right => left');
+        leftSlide(tab);
+      }
+  })
 })();
