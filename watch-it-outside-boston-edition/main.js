cceeb4661951d5a6debb64f34be7425ea0866a51
Elbert Castañeda <elbert@sbxretail.com>
2019-11-13 14:05:22 -0600

    Finish project film festival

--- watch-it-outside-boston-edition/main.js
+++ watch-it-outside-boston-edition/main.js
@@ -1,32 +1,30 @@
 (function() {
   'strict mode';
-  $('a.nav-link[href^="#"]:not([href="#"]),a.dropdown-item[href^="#"]', 'header').click('click', function(event) {
-    var href = $(this).attr('href');
+  function clickSubMenu(href) {
     var $href = $("[href='"+ href +"'][role='tab']");
+
+    // Trigger click show tab and refresh spy scroll
     $href.trigger('click');
 
-    setTimeout(function () {
-      scrollToHref(href);
-    }, 200);
-  });
+    scrollToHref(href);
+  }
 
   function scrollToHref (href) {
     var $section2Scroll = $(href);
+    if (href.indexOf("#movies") == 0) {
+      $section2Scroll = $('[id^="movies-"]:visible');
+    }
+
     var $main = $('main');
     var sectionTop = $section2Scroll.offset().top - 180;
     var currentScroll = $main.scrollTop();
     var moveScrollStopTo = sectionTop + currentScroll;
 
     $('.navbar-collapse').collapse('hide');
-
+    console.info(moveScrollStopTo)
     $main.animate({
       scrollTop: moveScrollStopTo
-    }, Math.abs(currentScroll -  moveScrollStopTo), function() {
-      // Patch to fix scrollspy
-      $main.scrollTop($main.scrollTop() - 50);
-      $main.scrollTop($main.scrollTop() + 50);
-      $('main[data-spy="scroll"]').scrollspy('refresh');
-    });
+    }, Math.abs(currentScroll -  moveScrollStopTo));
   }
 
   function leftSlide(tab){
@@ -41,21 +39,38 @@
     $tab.addClass('animated slideInRight');
   }
 
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-      var url = String(e.target);
-      var $fromTarget = $(e.relatedTarget);
-      var $toTarget = $(e.target);
-      var fromDayEvent = Number($fromTarget.attr('id').replace(/(movies-)|(th-tab)/g, ''));
-      var toDayEvent = Number($toTarget.attr('id').replace(/(movies-)|(th-tab)/g, ''));
-      // console.log(fromDayEvent, toDayEvent);
-      var tab = $(this).attr('href');
+  if (location.hash && location.hash.indexOf("#movies-") === 0) {
+    clickSubMenu(location.hash);
+  }
 
-      if (fromDayEvent < toDayEvent){
-        rightSlide(tab);
-        // console.log('left => right');
-      } else {
-        // console.log('right => left');
-        leftSlide(tab);
-      }
-  });
+  $('a.nav-link[href^="#"]:not([href="#"]),a.dropdown-item[href^="#"]', 'header')
+    .click('click', function(event) {
+      var href = $(this).attr('href');
+      clickSubMenu(href);
+    });
+
+  $('a[data-toggle="tab"]')
+    .on('shown.bs.tab', function (e) {
+        var url = String(e.target);
+        var $fromTarget = $(e.relatedTarget);
+        var $toTarget = $(e.target);
+        var fromDayEvent = Number($fromTarget.attr('id').replace(/(movies-)|(th-tab)/g, ''));
+        var toDayEvent = Number($toTarget.attr('id').replace(/(movies-)|(th-tab)/g, ''));
+        // console.log(fromDayEvent, toDayEvent);
+        var tab = $(this).attr('href');
+
+        if (fromDayEvent < toDayEvent){
+          rightSlide(tab);
+          // console.log('left => right');
+        } else {
+          // console.log('right => left');
+          leftSlide(tab);
+        }
+        setTimeout(function() {
+          var $main = $('main');
+          $main.scrollTop($main.scrollTop() - 50);
+          $main.scrollTop($main.scrollTop() + 50);
+          $('main[data-spy="scroll"]').scrollspy('refresh');
+        }, 200);
+    });
 })();
